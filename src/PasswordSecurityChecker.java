e6206c856d003972b457cd6ed5c15bde89718fb9
ErikPelloni <erik.pelloni@samtrevano.ch>
2021-09-30 14:24:44 +0200

    creazione classe java

---
+++ src/PasswordSecurityChecker.java
@@ -0,0 +1,12 @@
+/**
+ * La classe PasswordSecurityChecker contiene i metodi per verificare,
+ * in base a dei dati passati dall'utente, la sicurezza della password.
+ * Questa verifica viene eseguita tentando di forzare la password tramite
+ * combinazioni e tramite il confronto con una lista di password più comuni.
+ * @author Erik Pelloni
+ * @version 1.0 (30.09.2021)
+ */
+
+public class PasswordSecurityChecker{
+    
+}
