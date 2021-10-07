4a1c4bfc79eab9163ae5c2a181547a123207dd6f
ErikPelloni <erik.pelloni@samtrevano.ch>
2021-10-07 16:29:07 +0200

    mancano solo i test poi loadData è ok

--- Diari/Diario_PSC_2021_10_07.md
+++ Diari/Diario_PSC_2021_10_07.md
@@ -10,16 +10,14 @@
 |11:05 - 11:25 |Creazione nuovo diario                                          |
 |11:25 - 12:20 |Controllo validità dati input                                   |
 |13:20 - 14:25 |Controllo validità dati input                                   |
-|15:20 - 15:25 |Salvataggio dati input                                          |
-|15:25 - 16:10 |Metodo help e manuale                                           |
-|16:10 - 16:15 |Agiornamento diario                                             |
-|16:15 - 16:30 |Inizio controllo validità dei dati                              |
+|14:30 - 16:30 |Salvataggio dati input                                          |
 
 [//]: <> (##  Problemi riscontrati e soluzioni adottate)
 
 
 ##  Punto della situazione rispetto alla pianificazione
-
+Sono in linea con la progettazione iniziale, le ore che avevo guadagnato le ho già riutilizzate
 ## Programma di massima per la prossima giornata di lavoro
-+ Terminare la lettura, il controllo e il salvataggio degli input
-+ Iniziare l'algoritmo di ricerca semplice
++ Finire velocissimo i test per loadData
++ Iniziare la ricerca semplice
+AGGIORNARE DIAGRAMMA DELLE CLASSI
--- src/PasswordSecurityChecker.java
+++ src/PasswordSecurityChecker.java
@@ -1,6 +1,10 @@
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Date;
 
 /**
  * La classe PasswordSecurityChecker contiene i metodi per verificare,
@@ -12,13 +16,25 @@ import java.util.List;
  */
 
 public class PasswordSecurityChecker{
-    private String[] name;
+    private List<String> name;
     private int[] birth;
-    private String[] data;
+    private List<String> data;
     private char[] specials;
     private String password;
     private List<String> commons;
     private int tries;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+
+    /**
+     *Costruttore vuoto per poter istanziare un oggetto PasswordSecurityChecker
+     */
+    private PasswordSecurityChecker(){
+        name = new ArrayList<>();
+        data = new ArrayList<>();
+        birth = new int[3];
+        dateFormat.setLenient(false);
+    }
 
     /**
      * Il metodo displayHelp stampa il messaggio di help a terminale
@@ -34,11 +50,11 @@ public class PasswordSecurityChecker{
 
     /**
      * Il metodo checkData controlla se i dati passati da linea di comando
-     *  sono validi
+     * sono validi
      * @param args argomenti passati da linea di comando
-     * @return true se i dati passati sono corretti
+     * @return {@code true} se i dati passati sono corretti
      */
-    private static boolean checkData(String[] args){
+    private boolean checkData(String[] args){
         check:
         if(args.length >= 3){
             // controllo che non sia richiesto l'help
@@ -50,31 +66,53 @@ public class PasswordSecurityChecker{
             }
             // controllo la validità del nome
             if(args[0].split(" ").length <= 1){
-                System.out.println("Incorrect name");
+                System.err.println("Incorrect name");
                 break check;
             }
-            // controllo validità data di nascita
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-            dateFormat.setLenient(false);
-            try {
-                dateFormat.parse(args[1]);
-            } catch (ParseException pe) {
-                System.out.println("Incorrect birth date");
-                break check;
-            }
+            /**
+             * Il controllo della validità della data di nascita viene eseguito
+             * nel metodo loadData per evitare la ridondanza di codice
+            */
             return true;
         }
         displayHelp();
         return false;
     }
 
-    private static void loadData(String[] args){
+    /**
+     * Il metodo loadData salva i valori passati come input all'interno degli
+     * attributi dell'oggetto PasswordSecurityChecker
+     * @param args argomenti passati da linea di comando
+     */
+    private void loadData(String[] args){
         if(checkData(args)){
+            name = Arrays.asList(args[0].split(" "));
+            try {
+                Date birthDate = dateFormat.parse(args[1]);
+                Calendar c = Calendar.getInstance();
+                c.setTime(birthDate);
+                birth[0] = c.get(Calendar.DAY_OF_MONTH);
+                birth[1] = c.get(Calendar.MONTH) + 1;
+                birth[2] = c.get(Calendar.YEAR);
 
+            } catch (ParseException pe) {
+                System.err.println("Incorrect birth date");
+                displayHelp();
+                System.exit(0);
+            }
+            data = Arrays.asList(args[2].split(" "));
         }
     }
 
+
+    private void checkEasy() {
+        
+    }
     public static void main(String[] args) {
-        checkData(args);
+        PasswordSecurityChecker psc = new PasswordSecurityChecker();
+        psc.loadData(args);
+        for (int i : psc.birth) {
+            System.out.println(i);
+        }
     }
 }
