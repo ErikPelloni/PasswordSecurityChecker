4676c2a894be7df78d121bd2476a9149354cc275
ErikPelloni <erik.pelloni@samtrevano.ch>
2021-10-14 14:07:52 +0200

    aggiunto metodo per trovare le combinazioni

--- src/PasswordSecurityChecker.java
+++ src/PasswordSecurityChecker.java
@@ -3,36 +3,34 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
-
-import javax.lang.model.util.ElementScanner14;
-
 import java.util.Date;
 
 /**
- * La classe PasswordSecurityChecker contiene i metodi per verificare,
- * in base a dei dati passati dall'utente, la sicurezza della password.
- * Questa verifica viene eseguita tentando di forzare la password tramite
- * combinazioni e tramite il confronto con una lista di password più comuni.
+ * La classe PasswordSecurityChecker contiene i metodi per verificare, in base a
+ * dei dati passati dall'utente, la sicurezza della password. Questa verifica
+ * viene eseguita tentando di forzare la password tramite combinazioni e tramite
+ * il confronto con una lista di password più comuni.
+ * 
  * @author Erik Pelloni
  * @version 1.0 (30.09.2021)
  */
 
-public class PasswordSecurityChecker{
+public class PasswordSecurityChecker {
     private List<String> name;
     private int[] birth;
     private List<String> data;
-    private char[] specials = {'!','&', '?', '+', '%', '$', '-', '_'};
+    private char[] specials = { '!', '&', '?', '+', '%', '$', '-', '_', '@' };
     private String password;
     private List<String> commons;
     private int tries;
     private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
-
     /**
-     *Costruttore vuoto per poter istanziare un oggetto PasswordSecurityChecker
+     * Costruttore vuoto per poter istanziare un oggetto PasswordSecurityChecker
      */
-    private PasswordSecurityChecker(){
+    private PasswordSecurityChecker() {
         name = new ArrayList<>();
         data = new ArrayList<>();
         birth = new int[3];
@@ -42,42 +40,38 @@ public class PasswordSecurityChecker{
     /**
      * Il metodo displayHelp stampa il messaggio di help a terminale
      */
-    private static void displayHelp(){
-        System.out.println("Password Security Checker\n\nUsage: " + 
-                            "java PasswordSecurityChecker [<-h>] "+
-                            "<\"name surname\"> <\"dd.mm.yyyy\">"+
-                            " <\"information\"> <\"password\">"+
-                            "\n\nCheck the guide for more information " + 
-                            "about the program and formatting");
+    private static void displayHelp() {
+        System.out.println("Password Security Checker\n\nUsage: " + "java PasswordSecurityChecker [<-h>] "
+                + "<\"name surname\"> <\"dd.mm.yyyy\">" + " <\"information\"> <\"password\">"
+                + "\n\nCheck the guide for more information " + "about the program and formatting");
     }
 
     /**
-     * Il metodo checkData controlla se i dati passati da linea di comando
-     * sono validi
+     * Il metodo checkData controlla se i dati passati da linea di comando sono
+     * validi
+     * 
      * @param args argomenti passati da linea di comando
      * @return {@code true} se i dati passati sono corretti
      */
-    private boolean checkData(String[] args){
-        check:
-        if(args.length > 3){
+    private boolean checkData(String[] args) {
+        check: if (args.length > 3) {
             // controllo che non sia richiesto l'help
             for (String s : args) {
-                if(s.equals("-h") || s.equals("-help") || s.equals("h") 
-                || s.equals("help")){
+                if (s.equals("-h") || s.equals("-help") || s.equals("h") || s.equals("help")) {
                     break check;
                 }
             }
             // controllo la validità del nome
-            if(args[0].split(" ").length <= 1){
+            if (args[0].split(" ").length <= 1) {
                 System.err.println("Incorrect name\n");
                 break check;
             }
             /**
-             * Il controllo della validità della data di nascita viene eseguito
-             * nel metodo loadData per evitare la ridondanza di codice
-            */
+             * Il controllo della validità della data di nascita viene eseguito nel metodo
+             * loadData per evitare la ridondanza di codice
+             */
             return true;
-        }else{
+        } else {
             System.err.println("Missing arguments\n");
         }
         displayHelp();
@@ -87,10 +81,11 @@ public class PasswordSecurityChecker{
     /**
      * Il metodo loadData salva i valori passati come input all'interno degli
      * attributi dell'oggetto PasswordSecurityChecker
+     * 
      * @param args argomenti passati da linea di comando
      */
-    private void loadData(String[] args){
-        if(checkData(args)){
+    private void loadData(String[] args) {
+        if (checkData(args)) {
             name = Arrays.asList(args[0].split(" "));
             try {
                 Date birthDate = dateFormat.parse(args[1]);
@@ -111,14 +106,61 @@ public class PasswordSecurityChecker{
         }
     }
 
-
     private void checkEasy() {
-        
+        StringBuilder current = new StringBuilder();
+        String firstName = name.get(0);
+
     }
+
+    private void tryPassword(List<String> elements) {
+        StringBuilder s = new StringBuilder();
+        for (String string : elements) {
+            s.append(string);
+        }
+        tries++;
+        if(s.toString().equals(password)){
+            // trovata
+            // stop al tempo
+            // metodo finale
+            System.out.println("ciao mondo");
+        }
+    }
+
+
+    /** 
+     * la base del seguente metodo è stata presa dal sito
+     * https://www.baeldung.com/java-array-permutations
+     */
+
+    /**
+     * Il metodo prova tutte le combinazioni possibili
+     * @param elements lista di stringhe da controllare
+     */
+    private void tryAllPermutations(List<String> elements){
+        int[] indexes = new int[elements.size()];
+        tryPassword(elements);
+
+        int i = 0;
+        while (i < elements.size()) {
+            if (indexes[i] < i) {
+                Collections.swap(elements, i % 2 == 0 ?  0 : indexes[i], i);
+                tryPassword(elements);
+                indexes[i]++;
+                i = 0;
+            }
+            else {
+                indexes[i] = 0;
+                i++;
+            }
+        }  
+    }
+
     public static void main(String[] args) {
         PasswordSecurityChecker psc = new PasswordSecurityChecker();
         // controllo argomenti già eseguito
         psc.loadData(args);
-        
+        String[] arrayStrings = {"a", "b", "c", "d", "e", "f"};
+        List<String> elements = Arrays.asList(arrayStrings);
+        psc.tryAllPermutations(elements);
     }
 }
